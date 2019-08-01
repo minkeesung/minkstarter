000eab886b8cd2fb4cd64fc9d10c5eaf0d579276
minkee sung <minkeesung@minkees-MacBook-Air.local>
2019-08-01 17:05:35 -0400

    hard reset

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,6 @@ class Api::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    binding.pry
     @user = User.new(user_params)
 
     if @user.save
